61402ee07fca1bca5e1c70a0412e60c56a057e34
Jordan-Sage <60713325+Jordan-Sage@users.noreply.github.com>
2021-05-05 16:40:41 -0400

    up

---
+++ cos470final/Assets/Scripts/Cans.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cans : MonoBehaviour
+{
+    public int scoreValue = 0;
+    public Text displayText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("We in the can method");
+        if (collider.gameObject.tag == "can") {
+            
+            TheLogicAndStuff.GetInstance().score += this.scoreValue;
+
+            displayText.text = "SCORE:"+TheLogicAndStuff.GetInstance().score;
+            
+            Debug.Log(TheLogicAndStuff.GetInstance().score);
+            Destroy(collider.gameObject);
+        }
+    }
+}
---
+++ cos470final/Assets/Scripts/TheLogicAndStuff.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TheLogicAndStuff : MonoBehaviour
+{
+
+    public Text displayText;
+    public int cans = 0;
+    public int bottles = 0;
+    public int glass = 0;
+    public static TheLogicAndStuff _instance;
+
+    void Awake() {
+        if (_instance == null) {
+        _instance = this;
+        } else {
+        Destroy(gameObject);
+        }
+    }
+    
+    public static TheLogicAndStuff GetInstance() {
+        return TheLogicAndStuff._instance;
+    }
+
+}
