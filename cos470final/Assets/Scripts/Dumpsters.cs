b8b99dc71d6bf664217a8ad328f458dedccbad2d
Jordan-Sage <60713325+Jordan-Sage@users.noreply.github.com>
2021-05-05 16:27:05 -0400

    up

---
+++ cos470final/Assets/Scripts/Dumpsters.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dumpsters : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        Debug.Log("We in the method");
+        Debug.Log(collider.gameObject.tag == "crate");
+        if (collider.gameObject.tag == "crate") {
+            
+            Destroy(collider.gameObject);
+        }
+    }
+}
