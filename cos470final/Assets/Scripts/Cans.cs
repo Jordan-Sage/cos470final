1d128cb694a0796a5a25df54a1c52eb367b123e5
Jordan-Sage <60713325+Jordan-Sage@users.noreply.github.com>
2021-05-05 22:15:38 -0400

    up

--- cos470final/Assets/Scripts/Cans.cs
+++ cos470final/Assets/Scripts/Cans.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 
 public class Cans : MonoBehaviour
 {
-    public AudioSource audioSource = GetComponent<AudioSource>();
+    public AudioSource audioSource;
     public AudioClip clip;
     public Text displayText;
     public float volume = 0.5f;
@@ -13,7 +13,7 @@ public class Cans : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // audioSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -31,15 +31,14 @@ public class Cans : MonoBehaviour
             TheLogicAndStuff.GetInstance().cans += 1;
             Debug.Log(TheLogicAndStuff.GetInstance().cans);
             displayText.text = "Yup, that's a can";
-            audioSource.PlayOneShot(clip, volume);
+         
+        //    audioSource.PlayOneShot(clip, volume);
             
         }
-        if (collider.gameObject.tag == "bottle") {
-            displayText.text = "That looks like a " + collider.gameObject.tag;
-            audioSource.PlayOneShot(clip, volume);
+        if (collider.gameObject.tag == "bin") {
             
         } else {
-            displayText.text = "That looks like a " + collider.gameObject.tag;
+            displayText.text = "That looks like a " + collider.gameObject.tag + " ya dummy";
             Debug.Log(collider.gameObject.tag);
         }
     }
